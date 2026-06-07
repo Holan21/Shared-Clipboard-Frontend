26afb9f7390819f47d2b14fb9eb001897adae178
Holan21 <anton.kramarenko.holan@gmail.com>
2026-06-07 15:03:12 +0300

    Cleanup code

---
+++ Pages/Loginpage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared_Clipboard_Frontend
+{
+    public partial class LoginPage : ContentPage 
+    {
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
