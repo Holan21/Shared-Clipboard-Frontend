f59891dada5081f2531a7b8d76b90b61fde4b1aa
Holan21 <anton.kramarenko.holan@gmail.com>
2026-06-08 11:49:22 +0300

    Formating code

--- Pages/Loginpage.xaml.cs
+++ Pages/Loginpage.xaml.cs
@@ -5,11 +5,11 @@ namespace Shared_Clipboard_Frontend
 
     public partial class LoginPage : ContentPage
     {
-
         public LoginPage()
         {
             InitializeComponent();
         }
+
         private void OnPressLoginButton(object sender, EventArgs e)
         {
             var button = sender as Button;
