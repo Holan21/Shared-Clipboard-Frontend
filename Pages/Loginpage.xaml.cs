93f0f66b421d12b3d4c0e2028eb047c23d6932fd
Holan21 <anton.kramarenko.holan@gmail.com>
2026-06-08 11:48:32 +0300

    Added LoginPage

--- Pages/Loginpage.xaml.cs
+++ Pages/Loginpage.xaml.cs
@@ -1,11 +1,25 @@
-﻿namespace Shared_Clipboard_Frontend
+﻿using System.Diagnostics;
+
+namespace Shared_Clipboard_Frontend
 {
-    public partial class LoginPage : ContentPage 
+
+    public partial class LoginPage : ContentPage
     {
 
         public LoginPage()
         {
             InitializeComponent();
+        }
+        private void OnPressLoginButton(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            Debug.Print("Pressed");
+        }
+
+        private void OnRealesedLoginButton(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            Debug.Print("Realsed");
         }
     }
 }
